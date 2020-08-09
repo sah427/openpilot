7932ca08856b57420bcc7628edb34d0beb977ef0
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-08-09 11:56:15 -0400

    update

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -55,13 +55,12 @@ class CarState(CarStateBase):
 
     if self.CP.carFingerprint in ELEC_VEH:
       ret.gas = cp.vl["E_EMS11"]['Accel_Pedal_Pos'] / 256.
-      ret.gasPressed = ret.gas > 0
     elif self.CP.carFingerprint in HYBRID_VEH:
       ret.gas = cp.vl["EV_PC4"]['CR_Vcu_AccPedDep_Pc']
-      ret.gasPressed = ret.gas > 0
     else:
       ret.gas = cp.vl["EMS12"]['PV_AV_CAN'] / 100
-      ret.gasPressed = bool(cp.vl["EMS16"]["CF_Ems_AclAct"])
+
+    ret.gasPressed = (cp.vl["TCS13"]["DriverOverride"] == 1)
 
     # TODO: refactor gear parsing in function
     # Gear Selection via Cluster - For those Kia/Hyundai which are not fully discovered, we can use the Cluster Indicator for Gear Selection,
